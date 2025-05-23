dd08d6f65d8f2f5b7be6a7ba65b46b36b191b4a0
Paweł Walczykiewicz <pwalczykiewicz93@gmail.com>
2025-05-23 14:40:02 +0200

    Mobile menu changes

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    <UContainer class="flex items-center justify-center py-6 mb-2">
-      Portfolio created using
-      Vue.js 3 <UIcon name="i-vscode-icons-file-type-vue" :size="18" class="text-primary ml-1"/>,
+    <UContainer class="sm:flex sm:items-center sm:justify-center text-center py-6 mb-2">
+      Portfolio created using Vue.js 3 <UIcon name="i-vscode-icons-file-type-vue" :size="18" class="text-primary ml-1"/>,
       Nuxt 3 <UIcon name="i-vscode-icons-file-type-nuxt" :size="24" class="text-primary ml-1"/>, 
       and Tailwind CSS <UIcon name="i-vscode-icons-file-type-tailwind" :size="24" class="text-primary ml-1"/>
     </UContainer>
